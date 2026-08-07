8fe6b403d47b1ab9aac9b1dba9e91a3649baab47
LucaMalighetti <malighetti.luca.08@itisdalmine.edu.it>
2026-08-07 11:21:08 +0200

    Update Gmail App Password for email verification

--- server_example.js
+++ server_example.js
@@ -17,7 +17,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'malighettiluca08@gmail.com',
-        pass: 'lycm kghc wuls ijyb'
+        pass: 'fowc ccjg dulh hfsi'
     }
 });
 
